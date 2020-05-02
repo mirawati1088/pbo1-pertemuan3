23091e1522c95b50cb5211ec5c85690e68186bbc
mirawati1088 <mira.dhibah@gmail.com>
2020-05-02 06:36:12 -0700

    Percabangan Swith

---
+++ src/Percabangan/Swith.java
@@ -0,0 +1,24 @@
+package Percabangan;
+
+public class Swith {
+    public static void main(String[] args) {
+        int jalanJalan=3;
+        switch (jalanJalan){
+            case 1:
+                System.out.println("Qmall");
+                break;
+            case 2:
+                System.out.println("Maunjun");
+                break;
+            case 3:
+                System.out.println("Pengantinan mantan");
+                break;
+            case 4:
+                System.out.println("pahumaan cari kodok");
+                break;
+            default:
+                System.out.println("pasar");
+                break;
+        }
+    }
+}
