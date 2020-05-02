66bf12ca3da7d4223dd1b93818eb8db2a6a2a4b4
mirawati1088 <mira.dhibah@gmail.com>
2020-05-02 05:41:38 -0700

    Operator Binary Aritmatika

---
+++ src/operator/OperatorBinary.java
@@ -0,0 +1,20 @@
+package operator;
+
+public class OperatorBinary {
+
+
+
+
+
+    String nama = "Mira Wati";
+    int jumlahSks = 24;
+    double indeksPrestasiKumulatif = 3.89;
+
+    public static void main(String[] args) {
+        System.out.println(9+3);
+        System.out.println(9-3);
+        System.out.println(9*3);
+        System.out.println(9%3);
+        System.out.println(9%2);
+    }
+}
